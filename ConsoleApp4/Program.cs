fdcd06d79ea5f7db89dc38839377253da76cfced
Ken Domino <kenneth.domino@domemtech.com>
2017-08-31 13:15:08 -0400

    Clean up simple working test.

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,33 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Runtime.Remoting.Messaging;
-using Campy.ControlFlowGraph;
+﻿using System.Runtime.InteropServices;
 using Campy.Types;
-using Swigged.LLVM;
 
 namespace ConsoleApp4
 {
     class Program
     {
-
-
         static void Main(string[] args)
         {
-
             var all = Campy.Types.Accelerator.GetAll();
-            int[] host_data = new[] { 1, 2, 3, 4, 5 };
-
-            GCHandle h = GCHandle.Alloc(host_data, GCHandleType.Pinned);
-            var p = h.AddrOfPinnedObject();
+            int[] data = new[] { 1, 2, 3, 4, 5 };
 
             Campy.Parallel.For(new Extent(5), idx =>
             {
-                host_data[idx] += 1;
+                data[idx] += 1;
             });
-            for (int i = 0; i < host_data.Length; ++i) System.Console.WriteLine(host_data[i]);
+            for (int i = 0; i < data.Length; ++i) System.Console.WriteLine(data[i]);
 
             // FUTURE.
             //int n = Bithacks.Power2(20);
