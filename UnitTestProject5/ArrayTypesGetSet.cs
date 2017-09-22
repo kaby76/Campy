6f059885419ed7e165d5119ffe13d28fddfe8d01
Ken Domino <kenneth.domino@domemtech.com>
2017-09-22 10:20:05 -0400

    Adding in new tests--which cause a crash.

--- UnitTestProject5/ArrayTypesGetSet.cs
+++ UnitTestProject5/ArrayTypesGetSet.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Campy.Types;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestProject5
@@ -10,6 +11,7 @@ namespace UnitTestProject5
         public void ArrayTypesGetSetT()
         {
             int n = 4;
+
             int[] t1 = new int[n];
             Campy.Parallel.For(n, i => t1[i] = i);
             for (int i = 0; i < n; ++i) if (t1[i] != i) throw new Exception("unequal");
@@ -25,6 +27,25 @@ namespace UnitTestProject5
             System.UInt16[] t4 = new ushort[n];
             Campy.Parallel.For(n, i => t4[i] = (ushort)(i + 1));
             for (int i = 0; i < n; ++i) if (t4[i] != (ushort)(i + 1)) throw new Exception("unequal");
+
+            int[] t5 = new int[n];
+            Campy.Parallel.For(n, i => t5[i] = t1[i] * 2);
+            for (int i = 0; i < n; ++i) if (t5[i] != t1[i] * 2) throw new Exception("unequal");
+
+            float[] t6 = new float[n];
+            Campy.Parallel.For(n, i => t6[i] = 0.1f * i + t2[i]);
+            for (int i = 0; i < n; ++i) if (t6[i] != 0.1f * i + t2[i]) throw new Exception("unequal");
+
+            double[] t7 = new double[n];
+            Campy.Parallel.For(n, i => t7[i] = 0.1f * i - t3[i]);
+            for (int i = 0; i < n; ++i) if (t7[i] != 0.1f * i - t3[i]) throw new Exception("unequal");
+
+            System.UInt16[] t8 = new ushort[n];
+            Campy.Parallel.For(n, (Index i) =>
+            {
+                t8[i] = (ushort) (t4[i] + i + 1);
+            });
+            for (int i = 0; i < n; ++i) if (t8[i] != (ushort)(t4[i] + i + 1)) throw new Exception("unequal");
         }
     }
 }
