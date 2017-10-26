4bb0a6fcbe94cd74a83f8a749162833b8b39d70a
Ken Domino <kenneth.domino@domemtech.com>
2017-10-26 09:52:56 -0400

    Moving runtime to Campy.Runtime. Rolling over changes from Blazor project.

---
+++ Campy.Runtime/corlib/System.Reflection/PropertyInfo.cs
@@ -0,0 +1,24 @@
+﻿namespace System.Reflection
+{
+    public abstract class PropertyInfo : MemberInfo
+    {
+#pragma warning disable 0649
+        private readonly Type _propertyType;
+#pragma warning restore 0649
+
+        public Type PropertyType => _propertyType;
+
+        public MethodInfo GetGetMethod()
+        {
+            return _ownerType.GetMethod("get_" + _name);
+        }
+
+        public MethodInfo GetSetMethod()
+        {
+            return _ownerType.GetMethod("set_" + _name);
+        }
+
+        public MethodInfo GetMethod => GetGetMethod();
+        public MethodInfo SetMethod => GetSetMethod();
+    }
+}
---
+++ Campy.Runtime/corlib/System.Reflection/TypeInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace System.Reflection
+{
+    public class TypeInfo : MemberInfo
+    {
+        private readonly Type _type;
+
+        internal TypeInfo(Type type) : base(type, type.Name)
+        {
+            _type = type;
+        }
+
+        public bool IsValueType => _type.IsValueType;
+    }
+}
