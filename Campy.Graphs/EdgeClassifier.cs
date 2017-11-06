5cc5e9da150cb33b2a2c1b66fce13971b0aa25ab
Ken Domino <kenneth.domino@domemtech.com>
2017-11-06 10:17:57 -0500

    Adding edge classification implementation, which is based on Boost implementation.

--- Campy.Graphs/EdgeClassifier.cs
+++ Campy.Graphs/EdgeClassifier.cs
@@ -1,10 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Campy.Utils;
 
 namespace Campy.Graphs
 {
     public class EdgeClassifier
     {
+        enum Color
+        {
+            Black,
+            Gray,
+            White
+        }
+
+        public enum Classification
+        {
+            Tree,
+            Forward,
+            Back,
+            Cross
+        }
+
+        private static void Visit<T, E>(
+            T u,
+            IGraph<T, E> graph,
+            Dictionary<T, Color> color,
+            Dictionary<T, int> d,
+            Dictionary<T, int> f,
+            Dictionary<E, Classification> classify,
+            ref int time)
+            where E : IEdge<T>
+        {
+            color[u] = Color.Gray;
+            time = time + 1;
+            d[u] = time;
+            foreach (var e in graph.SuccessorEdges(u))
+            {
+                if (color[e.To] == Color.White)
+                {
+                    classify[e] = Classification.Tree;
+                    Visit(e.To, graph, color, d, f, classify, ref time);
+                }
+                else if (color[e.To] == Color.Gray)
+                    classify[e] = Classification.Back;
+                else if (color[e.To] == Color.Black)
+                {
+                    if (d[u] < d[e.To])
+                        classify[e] = Classification.Forward;
+                    else classify[e] = Classification.Cross;
+                }
+            }
+
+            color[u] = Color.Black;
+            time = time + 1;
+            f[u] = time;
+        }
+
+        public static void Classify<T, E>
+            (IGraph<T, E> graph, T u, out Dictionary<E, Classification> result)
+            where E : IEdge<T>
+        {
+            Dictionary<T, Color> color = new Dictionary<T, Color>();
+            Dictionary<T, int> d = new Dictionary<T, int>();
+            Dictionary<T, int> f = new Dictionary<T, int>();
+            Dictionary<E, Classification> classify = new Dictionary<E, Classification>();
+
+            foreach (var v in graph.Vertices)
+                color[v] = Color.White;
+
+            int time = 0;
+            Visit(u, graph, color, d, f, classify, ref time);
+            foreach (var v in graph.Vertices)
+                if (color[v] == Color.White)
+                    Visit(v, graph, color, d, f, classify, ref time);
+
+            result = classify;
+        }
     }
 }
--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -39,11 +39,37 @@ namespace ConsoleApp4
         {
             Campy.Graphs.Digraph.test();
             Campy.Graphs.DFSPostorder.test();
+            string tiny = $@"
+13
+22
+ 4  2
+ 2  3
+ 3  2
+ 6  0
+ 0  1
+ 2  0
+11 12
+12  9
+ 9 10
+ 9 11
+ 7  9
+10 12
+11  4
+ 4  3
+ 3  5
+ 6  8
+ 8  6
+ 5  4
+ 0  5
+ 6  4
+ 6  9
+ 7  6
+";
+
+            Digraph G = new Digraph(tiny);
+            EdgeClassifier.Classify(G, 7, out Dictionary<DirectedEdge<int>, EdgeClassifier.Classification> result);
 
             StartDebugging();
-
-
-
 
             {
 
