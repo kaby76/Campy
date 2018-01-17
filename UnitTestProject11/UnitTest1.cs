b7d11c6c578847798b3e5c1878da4d84807b25e0
Ken Domino <kenneth.domino@domemtech.com>
2018-01-17 10:02:51 -0500

    Fixing Math test.

--- UnitTestProject11/UnitTest1.cs
+++ UnitTestProject11/UnitTest1.cs
@@ -9,11 +9,15 @@ namespace UnitTestProject11
         [TestMethod]
         public void TestMethod1()
         {
-            double[] buffer = new double[] {0.1, 0.2, 0.3};
+            double[] buffer = new double[] { 0.1, 0.2, 0.3 };
+            double[] buffer_before = new double[] { 0.1, 0.2, 0.3 };
             Campy.Parallel.For(buffer.Length, k =>
             {
                 buffer[k] = Math.Sin(buffer[k]);
             });
+            for (int i = 0; i < buffer.Length; ++i)
+                if (buffer[i] != Math.Sin(buffer_before[i]))
+                    throw new Exception();
         }
     }
 }
