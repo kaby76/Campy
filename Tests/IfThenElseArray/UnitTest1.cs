3a2fb982206f69b3c08d879a8e8cd36a8c0b2999
Ken Domino <kenneth.domino@domemtech.com>
2018-05-30 09:09:01 -0400

    Adding If-then-else test using just arrays, not List<>.

---
+++ Tests/IfThenElseArray/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace IfThenElseArray
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            int n = 4;
+            var t1 = new int[n];
+            Campy.Parallel.For(n, i =>
+            {
+                if (i % 2 == 0)
+                    t1[i] = i * 20;
+                else
+                    t1[i] = i * 30;
+            });
+            for (int i = 0; i < n; ++i)
+                if (i % 2 == 0)
+                {
+                    if (t1[i] != i * 20) throw new Exception();
+                }
+                else
+                {
+                    if (t1[i] != i * 30) throw new Exception();
+                }
+        }
+    }
+}
