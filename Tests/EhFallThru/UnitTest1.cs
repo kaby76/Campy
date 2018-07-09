9355260e0319b2b70d47e52cd746a211731470cc
Ken Domino <kenneth.domino@domemtech.com>
2018-07-09 10:39:29 -0400

    Adding in simple try unit test. Note, it does not test finally clause.

---
+++ Tests/EhFallThru/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace EhFallThru
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            int l = -1;
+            Campy.Parallel.For(3, i =>
+            {
+                int j = i;
+                int k = 0;
+                try
+                {
+                    int m = 1;
+                    k = -2 * i;
+                    l = 99;
+                }
+                catch (Exception e)
+                {
+                    j = -i;
+                }
+                finally
+                {
+                    k = j;
+                }
+            });
+            if (l != 99) throw new Exception();
+        }
+    }
+}
