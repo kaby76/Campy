0b5bb3e1a6cb587219df47251d36bad3d5076bf8
Ken Domino <kenneth.domino@domemtech.com>
2018-09-02 12:00:35 -0400

    Clean up.

--- clean-completely.sh
+++ clean-completely.sh
@@ -1,10 +1,8 @@
 #
-rm -rf /c/Users/kenne/AppData/Local/Temp/VisualStudioTestExplorerExtensions
 rm -rf */bin
 rm -rf */obj
 rm -rf packages
 rm -rf x64
-rm -rf .vs
 rm -rf Tests/*/bin
 rm -rf Tests/*/obj
 rm -rf Campy.Runtime/Corlib/bin
