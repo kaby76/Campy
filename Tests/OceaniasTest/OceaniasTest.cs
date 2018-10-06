7ee5e21f64b4be1913a081a303b0efcfb83288a2
Ken Domino <kenneth.domino@domemtech.com>
2018-10-06 14:43:34 -0400

    (1) Adding in Oceania's test. (2) Adding in System.Private.CoreLib from coreclr. Note, the CoreLib code needs to be rewritten so as in all C#, as much as possible, for portability to AMD.

---
+++ Tests/OceaniasTest/OceaniasTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OceaniasTestNS
+{
+    [TestClass]
+    public class OceaniasTestC
+    {
+        [TestMethod]
+        public void OceaniasTest()
+		{
+			int n = 10;
+			var rand = new Random();
+			double[] x = new double[n];
+			double[] y = new double[n];
+			for (int i = 0; i < n; i++)
+			{
+				x[i] = rand.NextDouble();
+			}
+
+			Campy.Parallel.For(n, i =>
+			{
+				y[i] = 1 / (1 + Math.Pow(Math.E, -x[i]));
+			});
+
+		    for (int i = 0; i < n; ++i)
+		        Assert.IsTrue(0 < y[i] && y[i] < 1);
+		}
+    }
+}
---
+++ Tests/OceaniasTest461/OceaniasTest461.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OceaniasTest461NS
+{
+    [TestClass]
+    public class OceaniasTest461C
+    {
+        [TestMethod]
+        public void OceaniasTest461()
+        {
+            int n = 10000;
+            var rand = new Random();
+            double[] x = new double[n];
+            double[] y = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                x[i] = rand.NextDouble();
+            }
+
+            Campy.Parallel.For(n, i =>
+            {
+                y[i] = 1 / (1 + Math.Pow(Math.E, -x[i]));
+            });
+
+	        for (int i = 0; i < n; ++i)
+		        Assert.IsTrue(0 < y[i] && y[i] < 1);
+	    }
+    }
+}
